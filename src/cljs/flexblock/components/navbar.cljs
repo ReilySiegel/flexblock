ee0cb8d57918bd70c9cd6e5fefaf9ed9018a6238
Reily Siegel <mail@reilysiegel.com>
2018-04-26 16:50:43 -0400

    Separated navbar from core.

---
+++ src/cljs/flexblock/components/navbar.cljs
@@ -0,0 +1,50 @@
+(ns flexblock.components.navbar
+  (:require [re-frame.core :as rf]
+            [flexblock.utils :as u]
+            [flexblock.components.password :as password]))
+
+(defn nav-link [uri title page]
+  [:li.nav-item
+   {:class (when (= page @(rf/subscribe [:page])) "active")}
+   [:a.nav-link {:href uri} title]])
+
+(defn page-button []
+  (let [page @(rf/subscribe [:page])]
+    (condp = page
+      :rooms    [:li
+                 {:on-click #(rf/dispatch [:set-active-page :students])}
+                 [:a "Students"]]
+      :students [:li
+                 {:on-click #(rf/dispatch [:set-active-page :rooms])}
+                 [:a "Sessions"]])))
+
+(defn navbar []
+  (let [loading (rf/subscribe [:loading])]
+    [:divs
+     [password/modal @(rf/subscribe [:user]) {:id "reset-password-modal"}]
+     [:nav     
+      [:div.nav-wrapper.purple
+       [:a.brand-logo.center 
+        {:on-click #(do (u/get-rooms)
+                        (u/get-users))
+         :style    {:cursor :pointer}}
+        "FlexBlock"]
+       [:ul.left
+        (if (:teacher @(rf/subscribe [:user]))
+          [page-button])]
+       [:ul.right
+        (if (empty? @(rf/subscribe [:token]))
+          [:li [:a.modal-trigger {:href "#login-modal"} "Login"]]
+          [:div 
+           [:li [:a.modal-trigger {:href "#reset-password-modal"}
+                 "Reset Password"]]
+           [:li [:a {:on-click (fn [_]
+                                 (rf/dispatch [:set-rooms []])
+                                 (rf/dispatch [:set-user {}])
+                                 (rf/dispatch [:set-token ""])
+                                 (rf/dispatch [:set-active-page :rooms]))}
+                 "Logout"]]])]
+       [:ul.left.hide-on-med-and-down]
+       (when (pos? @loading)
+         [:div.progress.purple.lighten-3
+          [:div.indeterminate.amber]])]]]))
---
+++ src/cljs/flexblock/components/password.cljs
@@ -0,0 +1,28 @@
+(ns flexblock.components.password
+  (:require [re-frame.core :as rf]
+            [flexblock.utils :as u]
+            [flexblock.components.input :as input]
+            [flexblock.components.modal :as modal]))
+
+(defn modal
+  "Shows a modal that allows a teacher to change the password of `user`."
+  [user opts]
+  (let [{:keys [id]
+         :or   {id (str "passwordmodal" (:id user))}}
+        opts]
+    ^{:key (:id user)} 
+    [modal/standard id
+     [:div.modal-content
+      [:h4.center.purple-text.text-lighten-3
+       (str "Reset Password for " (:name user))]
+      [:div.row [:div.col.l6.offset-l3.m12
+                 [input/input-rf-dispatch
+                  {:type        :password
+                   :placeholder "New Password"}
+                  "Password"
+                  :set-reset-password
+                  :reset-password]]]]
+     [:div.modal-footer
+      [:a.btn-flat.amber-text.waves-effect.waves-purple
+       {:on-click #(u/set-password (:id user))}
+       "Reset Password"]]]))
