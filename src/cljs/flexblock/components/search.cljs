bfc639db88be93f76c1abd8ee3493b5b447bedd7
Reily Siegel <mail@reilysiegel.com>
2018-07-05 22:26:09 -0400

    Fixed search and date bar layout.

--- src/cljs/flexblock/components/search.cljs
+++ src/cljs/flexblock/components/search.cljs
@@ -4,27 +4,26 @@
    [flexblock.components.emailer :as emailer]
    [flexblock.components.input :as input]))
 
-(defn search-bar [] 
+(defn search-bar []
   [:div.row
-   [:div.col.l6.m12.offset-l3
+   [:div.col.l6.s12.offset-l3
     [input/text
-     {:placeholder   "Search" 
+     {:placeholder   "Search"
       :dispatch-key  :set-search
       :sunscribe-key :search
       :on-change?    true}]]])
 
 
-(defn date-bar [] 
+(defn date-bar []
   [:div.row
    (if (:admin @(rf/subscribe [:user]))
      [:div.input-field
-      [:div.col.l4.m12.offset-l3 
+      [:div.col.l4.m8.s6.offset-l3
        [input/datepicker
         {:dispatch-key  :set-date
-         :subscribe-key :date}]] 
-      [:div.col.l1 [emailer/fab]]]
-     
-     [:div.col.l4.m12.offset-l4
+         :subscribe-key :date}]]
+      [:div.col.l2.m4.s6.center [emailer/fab]]]
+     [:div.col.l4.m8.s12.offset-l4.offset-m2
       [input/datepicker
        {:dispatch-key  :set-date
         :subscribe-key :date}]])])
