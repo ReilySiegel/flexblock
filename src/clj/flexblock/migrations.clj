52a7e9fad76f318e93bf66d826168bb76493c4da
Reily Siegel <mail@reilysiegel.com>
2018-08-05 22:23:22 -0400

    Simplify flexblock.migrations.

--- src/clj/flexblock/migrations.clj
+++ src/clj/flexblock/migrations.clj
@@ -2,48 +2,28 @@
   (:require [clojure.java.jdbc :as jdbc]))
 
 (def table-specs
-  "Table specifications that can be used with any database.
-  Specs for users_rooms are in a separate var, as postgres and h2
-  handle foreign key relationships differently."
-  [[:users
-    [[:id :serial :primary :key]
-     [:name "varchar(50)" :not :null]
-     [:passwordhash "character(98)" :not :null]
-     [:email "varchar(50)" :unique :not :null]
-     [:teacher :boolean :not :null]
-     [:admin :boolean :not :null]
-     [:advisor_id :integer :references "users(id)"]
-     [:class :integer]]]
-   [:rooms
-    [[:id :bigserial :primary :key]
-     [:description "varchar(250)" :not :null]
-     [:title "varchar(50)" :not :null]
-     [:date :date :not :null]
-     [:room_number :integer :not :null]
-     [:max_capacity :integer :not :null]
-     [:time "varchar(6)" :not :null]]]])
-
-(def postgres-table-specs
-  "Tables that only work with postgres."
-  [:users_rooms
-   [[:id :bigserial :primary :key]
-    [:users_id :bigint :references "users(id)" :not :null]
-    [:rooms_id :bigint :references "rooms(id)" :not :null]]])
-
-(def h2-table-specs
-  "Tables that only work with h2."
-  [:users_rooms
-   [[:id :bigserial :primary :key]
-    [:users_id :bigint :not :null]
-    [:rooms_id :bigint :not :null]
-    [:foreign :key "(users_id)" :references "users(id)"]
-    [:foreign :key "(rooms_id)" :references "rooms(id)"]]])
+  "Table specifications for the database."
+  {:users       [[:id :serial :primary :key]
+                 [:name "varchar(50)" :not :null]
+                 [:passwordhash "character(98)" :not :null]
+                 [:email "varchar(50)" :unique :not :null]
+                 [:teacher :boolean :not :null]
+                 [:admin :boolean :not :null]
+                 [:advisor_id :integer :references "users(id)"]
+                 [:class :integer]]
+   :rooms       [[:id :bigserial :primary :key]
+                 [:description "varchar(250)" :not :null]
+                 [:title "varchar(50)" :not :null]
+                 [:date :date :not :null]
+                 [:room_number :integer :not :null]
+                 [:max_capacity :integer :not :null]
+                 [:time "varchar(6)" :not :null]]
+   :users_rooms [[:id :bigserial :primary :key]
+                 [:users_id :bigint :not :null :references "users(id)"]
+                 [:rooms_id :bigint :not :null :references "rooms(id)"]]})
 
 (defn init-tables! [connection]
-  (doseq [[table spec] (conj table-specs
-                             (if (= "h2:mem" (:dbtype connection))
-                               h2-table-specs
-                               postgres-table-specs))]
+  (doseq [[table spec] table-specs]
     (try
       (jdbc/db-do-commands
        connection
@@ -53,10 +33,7 @@
          (:cause (Throwable->map t)))))))
 
 (defn destroy-tables! [connection]
-  (doseq [[table] (reverse (conj table-specs
-                                 (if (= "h2:mem" (:dbtype connection))
-                                   h2-table-specs
-                                   postgres-table-specs)))]
+  (doseq [[table] table-specs]
     (try
       (jdbc/db-do-commands
        connection
